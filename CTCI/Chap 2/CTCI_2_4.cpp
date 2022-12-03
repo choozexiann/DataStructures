7e414bc3718be062f0604013293056b8101c8414
choozexiann <99162756+zexianchoo@users.noreply.github.com>
2022-12-03 17:46:00 -0600

    2_4 incomplete, problem with repeated loop

--- CTCI/Chap 2/CTCI_2_4.cpp
+++ CTCI/Chap 2/CTCI_2_4.cpp
@@ -61,11 +61,12 @@ void insertNode(Node*& head, int input_data) {
 /**
  * insertNode - [inserts a node into list to replace head]
  * @param head [ptr to head of list]
- * @param input_node [ptr to the node to be added]
+ * @param sub_head [ptr to the head of list to be added]
+ * @param sub_end [ptr to the end of list to be added]
 */
-void insertNode(Node*& head, Node* input_node) {
-    input_node->next = head;
-    head = input_node;
+void insertNode(Node*& head, Node* sub_head, Node* sub_end) {
+    sub_end->next = head;
+    head = sub_head;
 }
 
 /** deleteList - [deletes the entire list given head]
@@ -89,37 +90,50 @@ void deleteList(Node* head) {
  * @param head [head of list]
  * @param pivot_value [ value to parition the list on]
 */
-void partitionList(Node* head, int pivot_value){
+void partitionList(Node*& head, int pivot_value){
 
     if (head == nullptr) { throw std::invalid_argument("head pointer is invalid!"); }
 
     Node* slow_ptr = head;
     Node* runner = slow_ptr;
-    bool found_pivot = false;
+
     // buffer to accumualte the elements which are greater than pivot value which will be inserted at end of LL.
     Node* buffer = nullptr;
 
     // stop iteration at last element
-    while(slow_ptr->next){
-        
-        // runner runs and checks for r elements in partition
-        while (runner != nullptr && runner->data <= pivot_value) {
-            if (runner->data == pivot_value) { 
-                if (found_pivot) { break; } 
-                else { found_pivot = true; }
-             }
+    while(slow_ptr->next != nullptr && slow_ptr != nullptr){
+
+        // passes if the slow ptr is lower than pivot value.
+        if (slow_ptr->data <= pivot_value) { slow_ptr = slow_ptr->next; continue; }
+
+        // runner runs and checks for l elements in partition. Returns when runner is last element in l element LL
+        while (runner->next != nullptr && runner->next->data >= pivot_value) {
             runner = runner->next;
         }
 
         // save sub LL to buffer and point slow to runner
-        if (runner != slow_ptr){
-            
+        if (runner != slow_ptr) {
 
+            // detach sub LL
+            Node* sub_ll = slow_ptr;
+            Node* temp = runner->next;
+            runner->next = nullptr;
+
+            // insert the subLL into bufer
+            insertNode(buffer, slow_ptr, runner);
+
+            // point slow_ptr into the next l element value.
+            slow_ptr = temp;
+            runner = slow_ptr;
         }
-
-        slow_ptr = slow_ptr->next;
+        else if (slow_ptr->next != nullptr && slow_ptr != nullptr) { slow_ptr = slow_ptr->next; runner = slow_ptr; }
     }
 
+
+    // join back the elements
+    slow_ptr->next = buffer;
+
+    head = slow_ptr;
 }
 
 
@@ -141,9 +155,14 @@ int main() {
     for (int i = 0; i < 10; i++) {
         insertNode(head, randomRange(0,10));
     }
+
     printList(head);
+    // int partition_val = randomRange(0,10);
+    int partition_val = 5;
+    std::cout << "\nparition val will be: " << partition_val << '\n';
+    partitionList(head, partition_val);
 
-
+    printList(head);
     deleteList(head);
 
     return 0;
