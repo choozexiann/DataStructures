7019a05f2032cc3ba7a4575def0dc1a4f3d10254
choozexiann <99162756+zexianchoo@users.noreply.github.com>
2023-01-02 23:20:44 -0500

    rename

---
+++ CTCI/Chap4/CTCI_4_1.cpp
@@ -0,0 +1,14 @@
+/*
+CTCI_4_1
+Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a
+route between two nodes.
+*/
+
+#include "include.h"
+
+/* ====================== BFS =============================*/
+
+
+
+
+/* ====================== DFS =============================*/
---
+++ CTCI/Chap4/Include.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "..\Chap3\CTCI_3_6\Node.h"
+#include "..\Chap3\CTCI_3_6\Queue.h"
+#include "..\Chap3\CTCI_3_6\SinglyLinkedNode.h"
+#include "..\Chap3\CTCI_3_6\Stack.h"
